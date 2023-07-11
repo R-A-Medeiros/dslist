52f68de18e0f58e22d219377d8fdad1984e67ceb
Rinaldo <r.medeiros1206@gmail.com>
2023-07-11 17:26:44 -0300

    Game seed

--- src/main/java/com/dev/dslist/entities/Game.java
+++ src/main/java/com/dev/dslist/entities/Game.java
@@ -24,7 +24,11 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shotDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
